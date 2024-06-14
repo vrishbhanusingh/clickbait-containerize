0904f872564074545f50540c6a830126e169b894
vrishbhanusingh <vrishbhanu28@gmail.com>
2024-06-14 08:04:20 +0530

    fixed prompt

--- src/app/actions/NeedWebSearch.ts
+++ src/app/actions/NeedWebSearch.ts
@@ -18,7 +18,7 @@ export async function validateNeedWebSearch(message:string, pdfcontext:string) {
     try {
         const { object: result } = await generateObject({
             model: openai('gpt-3.5-turbo'),
-          system: 'You determine whether the message in the MESSAGE BLOCK needs web search results. You are given the PDF CONTEXT BLOCK which has relavant information from the PDF of a research paper. Please determine whether the message can be answered with just the information in the PDF CONTEXT BLCOK or if it needs a web search for more information. The answer should be yes if it needs web search results and no if it does not need a web search. For questions related to the paper in the PDF it is beneficial if you get more information from the web.',
+          system: 'You are tasked to determine whether the message in the MESSAGE BLOCK needs web search results. You are given the PDF CONTEXT BLOCK which has relavant information from the PDF of a research paper. Please determine whether the message can be answered with just the information in the PDF CONTEXT BLOCK or if it needs a web search for more information. The answer should be yes if it needs web search results and no if it does not need a web search. For questions related to the paper in the PDF it is beneficial if you get more information from the web.',
           prompt: `START OF PDF CONTEXT BLOCK\n
           ${pdfcontext}
           END OF PDF CONTEXT BLOCK\n\n
